8905ff77f2c3e3279ebd675e61eda0f2c62879e8
BrunoFerreira94 <brunolpcdz@hotmail.com>
2016-02-19 14:04:41 -0200

    Aprimoramento da tela inicial

---
+++ scripts/movimentoPersonagensAbertura.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var input_states = preload("res://scripts/input_states.gd")
+var clique_esquerdo = input_states.new("clique_esquerdo")
+
+var mainscene = preload("res://scenes/main_scene.scn")
+var numPlayer
+
+func _ready():
+	set_process(true)
+	numPlayer = 0
+	
+func _process(delta):
+	var dkplayer = get_node("personagem")
+	var zeplayer = get_node("personagem 2")
+	var mariaplayer = get_node("personagem 3")
+	
+	
+	
+	if clique_esquerdo.check() == 1:
+		return null
+	if clique_esquerdo.check() == 1:
+		return null
+	if clique_esquerdo.check() == 1:
+		return null
