1d39b22d3994b88ec1f574672c60b9b855f99127
Joao Daniel <jdcz94@gmail.com>
2016-03-04 13:08:06 -0300

    Padronizando variaveis e métodos

--- scripts/Personagem.gd
+++ scripts/Personagem.gd
@@ -7,24 +7,24 @@ var input_states = preload("res://scripts/InputStates.gd")
 #Variáveis para controlar o personagem no chão e no ar
 export var player_Speed = 700
 var accel = 5
-var air_accel = 2
-export var jump_force = 700
+var airAccel = 2
+export var jumpForce = 700
 
-var raycast_down = null
-var current_Speed = Vector2(0,0)
+var raycastDown = null
+var currentSpeed = Vector2(0,0)
 var jumping = 0
-export var pos_parada = 11000
+export var posParada = 11000
 
 #Variáveis de estado para saber se o personagem está no chão ou no ar
 var PLAYERSTATE_PREV = ""
 var PLAYERSTATE = ""
 var PLAYERSTATE_NEXT = "ground"
-var altura_tela =0
-var largura_tela = 0
-var soltou_tecla = false
+var alturaTela =0
+var larguraTela = 0
+
 
 #Variável que controla a animação atual do personagem
-var anim_personagem = null
+var animPersonagem = null
 
 #Variável para os efeitos sonoros do personagem
 var somPersonagem = null
@@ -40,7 +40,7 @@ var ground_state = false
 
 #função que vê se o Raycast está colidindo ou não com o chão
 func is_on_ground():
-	if raycast_down.is_colliding():
+	if raycastDown.is_colliding():
 		return true
 	else:
 		return false
@@ -50,12 +50,12 @@ func _ready():
 	
 	#Variável recebendo o Raycast2D e adicionando uma 
 	#exceção para não colidir com o personagem
-	raycast_down = get_node("RayCast2D")
-	raycast_down.add_exception(self)
-	altura_tela = get_viewport_rect().size.height
-	largura_tela = get_viewport_rect().size.width
+	raycastDown = get_node("RayCast2D")
+	raycastDown.add_exception(self)
+	alturaTela = get_viewport_rect().size.height
+	larguraTela = get_viewport_rect().size.width
 	#Variável recebendo o nó de animação do personagem
-	anim_personagem = get_node("AnimationPlayer")	
+	animPersonagem = get_node("AnimationPlayer")	
 	somPersonagem = get_node("somPersonagem")
 	self.set_gravity_scale(30)
 	
@@ -72,7 +72,7 @@ func _fixed_process(delta):
 	#verifica animação do personagem e atualiza para a nova animação
 	if anim != anim_new:
 		anim_new = anim
-		anim_personagem.play(anim)
+		animPersonagem.play(anim)
 		
 	move(player_Speed,accel,delta)
 	morreu()
@@ -86,7 +86,7 @@ func morreu():
 	var pause = get_parent().get_node("HUD/pause")
 	
 	#se o personagem sair dos limite lateral esquerdo ou inferior, ele perde
-	if (get_pos().x < 0 or get_pos().y > altura_tela) && voce_perdeu.is_hidden():
+	if (get_pos().x < 0 or get_pos().y > alturaTela) && voce_perdeu.is_hidden():
 		pause.hide()
 		voce_perdeu.show()
 		somPersonagem.play("lose")
@@ -99,15 +99,15 @@ func morreu():
 #função responsavel pelo movimento do personagem
 func move(speed, accel, delta):
 	anim = "andando"
-	#lerp calcula a interpolação linear entre current_speed.x e speed
-	current_Speed.x = lerp(current_Speed.x, speed, accel * delta)
-	if get_pos().x > pos_parada and get_pos().x < pos_parada+1000:
-		set_linear_velocity(Vector2(current_Speed.x-400, get_linear_velocity().y))
-	elif get_pos().x > pos_parada+1000:
+	#lerp calcula a interpolação linear entre currentSpeed.x e speed
+	currentSpeed.x = lerp(currentSpeed.x, speed, accel * delta)
+	if get_pos().x > posParada and get_pos().x < posParada+1000:
+		set_linear_velocity(Vector2(currentSpeed.x-400, get_linear_velocity().y))
+	elif get_pos().x > posParada+1000:
 		set_linear_velocity(Vector2(0, get_linear_velocity().y))
 		anim = "pulando"
 	else:
-		set_linear_velocity(Vector2(current_Speed.x, get_linear_velocity().y))
+		set_linear_velocity(Vector2(currentSpeed.x, get_linear_velocity().y))
 
 #funcao que verifica a posição de ground do personagem e permite o salto
 func check_pulo(delta):		
@@ -115,13 +115,13 @@ func check_pulo(delta):
 		if ground_state == true:
 			jumping = 0		
 		if pulo.check() == 1 and jumping == 0:
-			set_axis_velocity(Vector2(0, -jump_force))
+			set_axis_velocity(Vector2(0, -jumpForce))
 			somPersonagem.play("8-bit-jump")
 			jumping = 1
 	else:
 		ground_state = false
 		if pulo.check() == 1 and jumping < 2:
-			set_axis_velocity(Vector2(0, -jump_force))
+			set_axis_velocity(Vector2(0, -jumpForce))
 			somPersonagem.play("8-bit-jump")
 			jumping += 1
 		
