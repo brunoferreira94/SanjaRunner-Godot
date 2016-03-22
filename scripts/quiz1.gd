711536b0e68e705b536f84123ab70231cb08472a
kazuhirodk <kazuhiro.kojio@hotmail.com>
2016-03-22 12:58:17 -0300

    Implementação incompleta de recuperar resposta de arquivos

--- scripts/quiz1.gd
+++ scripts/quiz1.gd
@@ -4,7 +4,11 @@ extends Node2D
 var player = [preload("res://scenes/personagens/personagem_menino_negro.xml"), preload("res://scenes/personagens/personagem_cadeirante.xml"), preload("res://scenes/personagens/personagem_menina_branca.xml")]
 var numPlayer
 var questions = ["questao1","questao2","questao3","questao4","questao5"]
-var respostas = ["resp1","resp2","resp3","resp4","resp5"]
+var respostas = [["resp11","resp12","resp13","resp14"],
+				["resp21","resp22","resp23","resp24"],
+				["resp31","resp32","resp33","resp34"],
+				["resp41","resp42","resp43","resp44"],
+				["resp51","resp52","resp53","resp54"]]
 
 func _ready():
 	set_process(true)
@@ -38,20 +42,34 @@ func set_question(id):
 				questions[j] = linha
 				print(questions[j],"\n")
 
-
+#pega o texto do arquivo e atribui para as respostas
 func set_resposta(id):
 	var respostas = File.new()
 	var linha
 	respostas.open("res://respostas.txt",File.READ)
 	while(!respostas.eof_reached()):
-		
+		for i in range (5):
+			for j in range (4):
+				linha = respostas.get_line()
+				respostas[i][j] = linha
 
 
-#atribui o texto para à questao
+#atribui o texto para o balão de questao
 func set_texto_questao(id):
 	get_node("TextBaloon/questao").add_text(questions[id])
+	
+
+#atribui o texto para os balões de resposta
+func set_texto_resposta(id):
+	get_node("TextBaloon/a/respA").add_text(respostas[id][0])
+	get_node("TextBaloon/b/respB").add_text(respostas[id][1])
+	get_node("TextBaloon/c/respC").add_text(respostas[id][2])
+	get_node("TextBaloon/d/respD").add_text(respostas[id][3])
+
 	
 func screen_question(id):
 	set_question(id)
 	set_texto_questao(id)
+	set_resposta(id)
+	set_texto_resposta(id)
 	
