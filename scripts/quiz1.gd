a9b1e9c5757fe306f0116d6da9b0b48efe6a60f1
kazuhirodk <kazuhiro.kojio@hotmail.com>
2016-03-28 12:58:06 -0300

    Tentativa de implementar metodo wait - imcompleto

--- scripts/quiz1.gd
+++ scripts/quiz1.gd
@@ -10,15 +10,55 @@ var respostas = [["resp11","resp12","resp13","resp14"],
 				["resp41","resp42","resp43","resp44"],
 				["resp51","resp52","resp53","resp54"]]
 var respostabool = [ [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0] ]
+var pontuacao
 
 func _ready():
 	
 	set_process(true)
 	spawnPlayer(145,600)
-	screen_question(4)
+	pontuacao = 0
+	
+	quizCompleto(0)
 
-
-
+func quizCompleto(id):
+	
+	screen_question(id)
+	
+	if get_node("TextBaloon/a").is_pressed():
+		if respostabool[id][0] == 1:
+			pontuacao = pontuacao + 1
+			id = id + 1
+			reset_question(id)
+		else: 
+			id = id + 1
+			reset_question(id)
+		
+	elif get_node("TextBaloon/b").is_pressed():
+		if respostabool[id][1] == 1:
+			pontuacao = pontuacao + 1
+			id = id + 1
+			reset_question(id)
+		else:
+			id = id + 1
+			reset_question(id)
+	
+	elif get_node("TextBaloon/c").is_pressed():
+		if respostabool[id][2] == 1:
+			pontuacao = pontuacao + 1
+			id = id + 1
+			reset_question(id)
+		else:
+			id = id + 1
+			reset_question(id)
+		
+	elif get_node("TextBaloon/d").is_pressed():
+		if respostabool[id][3] == 1:
+			pontuacao = pontuacao + 1
+			id = id + 1
+			reset_question(id)
+		else:
+			id = id + 1
+			reset_question(id)
 
 
 #dá spawn do ultimo personagem escolhido na posicao x,y do mapa
@@ -42,12 +82,12 @@ func set_question(id):
 	var questoes = File.new()
 	var linha
 	questoes.open("res://Quiz/quiz1/questoes.txt", File.READ)
-	while(!questoes.eof_reached()):
-		if questoes.get_line() == str(id):
-			for j in range(5):
-				linha = questoes.get_line()
-				questions[j] = linha
-				print(questions[j],"\n")
+	#while(!questoes.eof_reached()):
+		#if questoes.get_line() == str(id):
+	for j in range(5):
+		linha = questoes.get_line()
+		questions[j] = linha
+		print(questions[j],"\n")
 
 #pega o texto do arquivo e atribui para as respostas
 func set_resposta(id):
@@ -86,6 +126,15 @@ func set_texto_resposta(id):
 	get_node("TextBaloon/b/respB").add_text(respostas[id][1])
 	get_node("TextBaloon/c/respC").add_text(respostas[id][2])
 	get_node("TextBaloon/d/respD").add_text(respostas[id][3])
+	
+func reset_question(id):
+	get_node("TextBaloon/a/respA").clear()
+	get_node("TextBaloon/b/respB").clear()
+	get_node("TextBaloon/c/respC").clear()
+	get_node("TextBaloon/c/respC").clear()
+	get_node("TextBaloon/Pergunta/questao").clear()
+	set_texto_questao(id)
+	set_texto_resposta(id)
 
 #função para apresentar todas as informações na tela
 func screen_question(id):
