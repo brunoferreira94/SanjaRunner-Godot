65a2ec8f586cbdeec73aa014a0269186f7954927
yoji-kojio <yoji.kojio@gmail.com>
2016-04-05 09:45:26 -0300

    Seleção de personagens provisoria

---
+++ scripts/button_back.gd
@@ -0,0 +1,17 @@
+
+extends Control
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+
+
+func _on_sair_pressed():
+	get_tree().change_scene("res://scenes/Cena0_abertura.xml")
